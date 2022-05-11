b53c6ec5a164481519ce20973aa6de46bf9a4004
zhanglin <lin.zhang@cy-tech.net>
2022-05-11 15:21:53 +0800

    feat(commit.config.js): 修改commit-msg

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,32 @@
 module.exports = {
   extends: ['@commitlint/config-conventional']
+//   rules: {
+//     'body-leading-blank': [1, 'always'], // body开始于空白行
+//     'body-tense': [1, 'always', ['present-imperative']],
+//     'footer-leading-blank': [1, 'always'], // footer开始于空白行
+//     'footer-tense': [1, 'always', ['present-imperative']],
+//     'header-max-length': [2, 'always', 72], // 简述限制72字符长度
+//     'scope-case': [2, 'always', 'lowerCase'], // scope小写
+//     'subject-empty': [2, 'never'], // subject不为空
+//     'subject-full-stop': [2, 'never', '.'], // subject结尾不加'.'
+//     'subject-tense': [1, 'always', ['present-imperative']], // 以动词开头，使用第一人称现在时，比如change，而不是changed或changes
+//     'type-case': [2, 'always', 'lowerCase'], // type小写
+//     'type-empty': [2, 'never'], // type不为空
+//     'type-enum': [
+//       2,
+//       'always',
+//       [
+//         'build', // 修改构建打包文件
+//         'chore', // 构建过程或辅助工具的变动
+//         'docs', // 文档（documentation）
+//         'feat', // 新功能（feature）
+//         'fix', // 修补bug
+//         'perf', // 性能优化
+//         'refactor', // 重构（即不是新增功能，也不是修改bug的代码变动）
+//         'revert', // 还原以前的提交
+//         'style', // 格式（不影响代码运行的变动）
+//         'test' // 增加测试
+//       ]
+//     ] // type关键字必须是其中之一
+//   }
 }
